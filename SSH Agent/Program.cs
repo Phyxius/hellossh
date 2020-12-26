569a7273432923722315bdb50e2a71d7488b9cb1
Shea Polansky <shea@polansky.co>
2020-12-25 20:28:37 -0800

    Show tray icon when running

--- SSH Agent/Program.cs
+++ SSH Agent/Program.cs
@@ -1,11 +1,15 @@
 ﻿using HelloSSH.Agent;
 using System;
+using System.Windows.Forms;
+using System.Threading;
+using System.Drawing;
 
 namespace HelloSSH
 {
     class Program
     {
         const string DefaultConfigLocation = "helossh.json";
+        [STAThread]
         static void Main(string[] args)
         {
             if (args.Length > 1)
@@ -17,7 +21,23 @@ namespace HelloSSH
             {
                 configLocation = args[1];
             }
-            new HelloSSHAgent(new ConfigurationProvider(configLocation)).ListenOnNamedPipe();
+            Application.SetHighDpiMode(HighDpiMode.PerMonitorV2);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            var agent = new HelloSSHAgent(new ConfigurationProvider(configLocation));
+            new Thread(agent.ListenOnNamedPipe).Start();
+            var contextMenu = new ContextMenuStrip();
+            contextMenu.Items.Add("&Exit");
+            contextMenu.Items.Add("&About HelloSSH");
+            contextMenu.Items.Add("Open key &manager");
+            var icon = new NotifyIcon
+            {
+                Icon = Properties.Resources.ApplicationIcon,
+                Visible = true,
+                Text = "HelloSSH",
+                ContextMenuStrip = contextMenu
+            };
+            Application.Run();
         }
 
         private static void PrintUsageAndExit()
