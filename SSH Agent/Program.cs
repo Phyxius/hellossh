77bc456a7d80bc7425ef6cbd569b3b31acb4fcc9
Shea Polansky <shea@polansky.co>
2020-12-26 21:00:06 -0800

    Simplify entry point

--- SSH Agent/Program.cs
+++ SSH Agent/Program.cs
@@ -12,17 +12,22 @@ namespace HelloSSH
     {
         const string DefaultConfigLocation = "helossh.json";
 
-        [STAThread]
-        static void Run(string[] args)
+        private static void PrintUsageAndExit()
         {
-            if (args.Length > 1)
+            MessageBox.Show(null, @"Usage: heloossh.exe [C:\path\to\config.json]", "Usage", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            Environment.Exit(-1);
+        }
+
+        public void OnApplicationStart(object sender, System.Windows.StartupEventArgs e)
+        {
+            if (e.Args.Length > 1)
             {
                 PrintUsageAndExit();
             }
             var configLocation = DefaultConfigLocation;
-            if (args.Length > 0)
+            if (e.Args.Length > 0)
             {
-                configLocation = args[1];
+                configLocation = e.Args[1];
             }
             Application.SetHighDpiMode(HighDpiMode.PerMonitorV2);
             Application.EnableVisualStyles();
@@ -35,17 +40,6 @@ namespace HelloSSH
             };
             agentThread.Start();
             TrayIcon.CreateTrayIcon(dataStore);
-        }
-
-        private static void PrintUsageAndExit()
-        {
-            MessageBox.Show(null, @"Usage: heloossh.exe [C:\path\to\config.json]", "Usage", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            Environment.Exit(-1);
-        }
-
-        public void OnApplicationStart(object sender, System.Windows.StartupEventArgs e)
-        {
-            Run(e.Args);
         }
     }
 }
